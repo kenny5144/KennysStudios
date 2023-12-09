0ec27bcf78c8f1a0ba72d089d495a5f60edd78e3
kehinde ojo <ojok761@gmail.com>
2023-12-08 23:32:57 -0500

    removed date

--- PhotographyPage/src/Pages/Contact.jsx
+++ PhotographyPage/src/Pages/Contact.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 import contact from "../images/homepageimages/contact.jpg";
-import DatePicker from "react-datepicker";
-// import "react-datepicker/dist/react-datepicker.css"; 
+
 import emailjs from "@emailjs/browser";
 
 const Contact = () => {
